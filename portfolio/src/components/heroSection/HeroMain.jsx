4b1c3d1155064aee6ea9bfe49f93ab61fac2e4e5
Dhananjaya Rupasinghe <106276257+GWMDhananjaya@users.noreply.github.com>
2025-03-09 00:15:52 +0530

    create files  for herosection and conect those files.

---
+++ portfolio/src/components/heroSection/HeroMain.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import HeroPic from "./HeroPic";
+import HeroText from "./HeroText";
+
+const HeroMain = () => {
+  return (
+    <div>
+      <HeroText />
+      <HeroPic />
+    </div>
+  );
+};
+
+export default HeroMain;
