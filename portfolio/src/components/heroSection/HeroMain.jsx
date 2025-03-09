8cd7cc57e6bb7729775db776d823c85315654aa7
Dhananjaya Rupasinghe <106276257+GWMDhananjaya@users.noreply.github.com>
2025-03-09 14:01:58 +0530

    add main image (home ) and add text to home page

--- portfolio/src/components/heroSection/HeroMain.jsx
+++ portfolio/src/components/heroSection/HeroMain.jsx
@@ -4,7 +4,7 @@ import HeroText from "./HeroText";
 
 const HeroMain = () => {
   return (
-    <div>
+    <div className="pt-40 pb-16">
       <HeroText />
       <HeroPic />
     </div>
--- portfolio/src/components/heroSection/HeroPic.jsx
+++ portfolio/src/components/heroSection/HeroPic.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { FaCentercode } from "react-icons/fa";
 
 const HeroPic = () => {
-  return <div>HeroPic</div>;
+  return (
+    <div className="h-full flex item-center justify-center">
+      <img
+        src="../../../public/images/main-photoaidcom-cropped.jpg"
+        alt="dhananjaya image"
+        className="max-h-[350px] w-auto "
+      />
+      <div className="absolute -z-10 flex justify-center items-center animate-pulse ">
+        <FaCentercode className="md:h-[90%] sm:h-[120%] min-h-[360px] w-auto text-cyan blur-md animate-[spin_20s_linear_infinite]" />
+      </div>
+    </div>
+  );
 };
 
 export default HeroPic;
