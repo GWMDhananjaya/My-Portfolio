ad8d1703e56d99d5d8ea2820c1c6bf17a63ac3c1
Dhananjaya Rupasinghe <106276257+GWMDhananjaya@users.noreply.github.com>
2025-03-10 16:36:48 +0530

    pfinished about section with image  and 2 buttons

--- portfolio/src/components/navbar/NavbarBtn.jsx
+++ portfolio/src/components/navbar/NavbarBtn.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import { LuArrowDownRight } from "react-icons/lu";
+import { TbFileDownload } from "react-icons/tb";
 
 const NavbarBtn = () => {
   return (
     <button className="px-4 py-2 rounded-full text-xl font-bold text-white border-cyan borderr flex item-center gap-1 bg-gradient-to-r from-cyan to-orange hover:border-orange hover:scale-110 transition-all duration-500 hover:shadow-cyanShadow">
-      Me
-      <div className="sm:hidden md:block">
-        <LuArrowDownRight />
+      CV
+      <div className="sm:hidden md:block mt-0.5">
+        <TbFileDownload />
       </div>
     </button>
   );
