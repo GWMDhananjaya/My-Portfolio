866f193399b45bd55439b31c930ccbc18d9c23ec
Dhananjaya Rupasinghe <106276257+GWMDhananjaya@users.noreply.github.com>
2025-03-11 14:51:55 +0530

    make skillsection fils and make conection and make helpersection file

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -4,6 +4,8 @@ import HeroMain from "./components/heroSection/HeroMain";
 import HeroGradient from "./components/heroSection/HeroGradient";
 import SubHeroSection from "./components/heroSection/SubHeroSection";
 import AboutMeMain from "./components/aboutMeSection/AboutMeMain";
+import SkillsMain from "./components/skillsSection/SkillsMain";
+import HelperSection from "./components/HelperSection";
 
 function App() {
   return (
@@ -13,6 +15,8 @@ function App() {
       <HeroGradient />
       <SubHeroSection />
       <AboutMeMain />
+      <SkillsMain />
+      <HelperSection />
     </main>
   );
 }
